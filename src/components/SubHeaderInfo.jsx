29c2761c39aecf9c2c6af726f6521ac06e03254a
Amber Jankowski <amberbjankowski@gmail.com>
2024-08-30 12:08:48 -0400

    fixed some minor CSS issues with sizing and margins

--- src/components/SubHeaderInfo.jsx
+++ src/components/SubHeaderInfo.jsx
@@ -4,8 +4,8 @@ import "../styles/main.css";
 function SubHeaderInfo() {
   return (
     <div className="subHeaderInfoContainer interFontLight">
-      <h4>Call us: 941-204-3066</h4>
-      <h4>license #: CBC 1264369</h4>
+      <h4 className="interFont subInfo">Call us: 941-204-3066</h4>
+      <h4 className="interFont subInfo">license #: CBC 1264369</h4>
     </div>
   );
 }
