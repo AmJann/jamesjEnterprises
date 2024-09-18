94cc7699ac38baa8038c2ddbbb78ffc7bbfb7ae1
Amber Jankowski <amberbjankowski@gmail.com>
2024-09-18 15:31:04 -0400

    fixed a lot off css

--- src/components/Phone.jsx
+++ src/components/Phone.jsx
@@ -1,12 +1,24 @@
 import React from "react";
 import "../styles/contact.css";
+import manCalling from "../assets/manCalling.jpeg";
 
 function Phone() {
   return (
-    <div className="callUs">
-      <h2 className="montserratFont"> Call Us</h2>
-      <h3 className="montserratFont goldFontContact">941-204-3066</h3>
-      <h3 className="montserratFont">Weekdays 9am - 4pm</h3>
+    <div className="phoneContainer">
+      <a href="tel:+19412043066" className="phoneDiv">
+        <div className="callUs">
+          <h2 className="montserratFont"> Call Us</h2>
+          <h3 className="montserratFont goldFontContact">941-204-3066</h3>
+          <h3 className="montserratFont">Weekdays 9am - 4pm</h3>
+        </div>
+      </a>
+      <div className="manCallingContainer">
+        <img
+          className="manCallingImg"
+          src={manCalling}
+          alt="smiling man making a phone call looking out a home window"
+        ></img>
+      </div>
     </div>
   );
 }
--- src/components/ServicesList.jsx
+++ src/components/ServicesList.jsx
@@ -1,32 +1,76 @@
 import React from "react";
 import "../styles/main.css";
+import homeDriveway from "../assets/homeDriveway.jpeg";
+import patio from "../assets/patioBrown.jpg";
+import sidewalk from "../assets/sidewalk2.jpg";
+import concreteSlab from "../assets/concreteSlab.jpg";
 
 function ServicesList() {
   return (
-    <div className="interFontLight servicesListConatiner">
-      <h3 className="servicesCategory">Concrete Flatwork</h3>
-      <li className="servicesItem">Driveways</li>
-      <li className="servicesItem">Patios</li>
-      <li className="servicesItem">Building Slabs</li>
-      <li className="servicesItem">Sidewalks</li>
-      <br></br>
-      <h3 className="servicesCategory">Block Work</h3>
+    <div className="allServicesContainer">
+      <div className="interFontLight servicesListConatiner">
+        <h3 className="servicesCategory">Concrete Flatwork</h3>
+        <li className="servicesItem">Driveways</li>
+        <li className="servicesItem">Patios</li>
+        <li className="servicesItem">Building Slabs</li>
+        <li className="servicesItem">Sidewalks</li>
+        <br></br>
+        <h3 className="servicesCategory">Block Work</h3>
 
-      <li className="servicesItem">Stemwalls</li>
-      <li className="servicesItem">Additions</li>
-      <li className="servicesItem">Block Buildings</li>
-      <br></br>
-      <h3 className="servicesCategory">Structural Concrete</h3>
+        <li className="servicesItem">Stemwalls</li>
+        <li className="servicesItem">Additions</li>
+        <li className="servicesItem">Block Buildings</li>
+        <br></br>
+        <h3 className="servicesCategory">Structural Concrete</h3>
 
-      <li className="servicesItem">Tie Beam</li>
-      <li className="servicesItem">Stairs</li>
-      <li className="servicesItem">Wet Decks</li>
-      <br></br>
-      <h3 className="servicesCategory">Miscellaneous</h3>
+        <li className="servicesItem">Tie Beam</li>
+        <li className="servicesItem">Stairs</li>
+        <li className="servicesItem">Wet Decks</li>
+        <br></br>
+        <h3 className="servicesCategory">Miscellaneous</h3>
 
-      <li className="servicesItem">Culvert Pipe</li>
-      <li className="servicesItem">Rip-Rap Banks</li>
-      <li className="servicesItem">Brick Pavers</li>
+        <li className="servicesItem">Culvert Pipe</li>
+        <li className="servicesItem">Rip-Rap Banks</li>
+        <li className="servicesItem">Brick Pavers</li>
+      </div>
+      <div className="imagesServicesContainer">
+        <div className="imagesServicesSection">
+          <div className="imageItem">
+            <img
+              alt="home at sunset driveway"
+              src={homeDriveway}
+              className="imageService"
+            ></img>
+            <div className="overlayService">Driveway</div>
+          </div>
+          <div className="imageItem">
+            <img
+              src={concreteSlab}
+              alt="concrete home slab"
+              className="imageService"
+            ></img>
+            <div className="overlayService">Slab</div>
+          </div>
+        </div>
+        <div className="imagesServicesSection">
+          <div className="imageItem">
+            <img
+              src={sidewalk}
+              alt="concrete sidewalk"
+              className="imageService"
+            ></img>
+            <div className="overlayService">Sidewalk</div>
+          </div>
+          <div className="imageItem">
+            <img
+              alt="concrete patio"
+              src={patio}
+              className="imageService"
+            ></img>
+            <div className="overlayService">Patio</div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
