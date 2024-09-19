6aaaf0c0f9bf7f577d742cd8a068f32d7583a169
Amber Jankowski <amberbjankowski@gmail.com>
2024-09-19 12:40:36 -0400

    added google tag

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,28 @@ import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
 import Services from "./pages/Services";
 import Footer from "./components/Footer";
-import { React, Fragment } from "react";
+import { React, Fragment, useEffect } from "react";
 import { Routes, Route } from "react-router-dom";
 import Gallery from "./pages/Gallery";
 import Contact from "./pages/Contact";
-// import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
+  useEffect(() => {
+    const script1 = document.createElement("script");
+    script1.async = true;
+    script1.src = `https://www.googletagmanager.com/gtag/js?id=G-GQ8L7TVMG0`;
+    document.head.appendChild(script1);
+
+    const script2 = document.createElement("script");
+    script2.innerHTML = `
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+      gtag('config', 'G-GQ8L7TVMG0');
+    `;
+    document.head.appendChild(script2);
+  }, []);
+
   return (
     <Fragment>
       <Navbar />
